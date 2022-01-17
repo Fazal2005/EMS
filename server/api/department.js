8270a35e70240945f42fb94ee4338a841cbe4df8
Fazal <72785775+Fazal2005@users.noreply.github.com>
2022-01-17 16:27:58 +0530

    Add files via upload

---
+++ server/api/department.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const router = express.Router();
+
+const queries = require('../db/dep_queries');
+
+const {authDepartment} = require('../middlewares/middlewares');
+
+function isValidId(req, res, next) {
+    if(!isNaN(req.params.id)) return next();
+    next(new Error('Invalid ID'));
+}
+function validDepartment(department) {
+    const hasDepartmentname = typeof department.department_name == 'string' && department.department_name.trim() !='';
+    return hasDepartmentname;
+}
+
+
+router.get('/', authDepartment(["admin","superAdmin"]),(req, res) => {
+    queries.getAll().then(department => {
+        res.json(department);
+    });
+});
+
+router.get('/:id', isValidId, authDepartment(["admin","superAdmin"]),(req, res) => {
+    queries.getOne(req.params.id).then(department => {
+        res.json(department);  
+    });
+});
+
+router.post('/', authDepartment(["admin","superAdmin"]),(req, res, next) => {
+    if(validDepartment(req.body)) {
+        queries.create(req.body).then(department => {
+            res.json(department[0]);
+        });
+    } else {
+        next(new Error('invalid Department'));
+    }
+});
+
+router.put('/:id', isValidId, authDepartment(["admin","superAdmin"]),(req, res, next) => {
+    if(validDepartment(req.body)) {
+        queries.update(req.params.id, req.body).then(department => {
+            res.json(department);
+        });
+    } else {
+        next(new Error('Invalid Department'));
+    }
+});
+
+router.delete('/:id', isValidId, authDepartment(["admin","superAdmin"]),(req, res) => {
+    queries.delete(req.params.id).then(() => {
+        res.json({
+            deleted: true
+        })
+    });
+});
+
+module.exports = router; 
---
+++ server/migrations/20220116142136_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id');
+        table.string('name');
+        table.string('email');
+        table.string('role');
+        table.varchar('password');
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
